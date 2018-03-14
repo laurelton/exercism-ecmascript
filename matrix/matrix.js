17fe47c3d7782668fd09129624038d74c335934a
laurelton <777443+laurelton@users.noreply.github.com>
2018-03-14 18:30:37 -0400

    Matrix completed

---
+++ matrix/matrix.js
@@ -0,0 +1,19 @@
+class Matrix {
+  constructor(values) {
+    this._rows = values.split('\n').map(elem => {
+      return elem.split(' ').map(str => parseInt(str));
+    });
+
+    this._cols = this._rows[0].map((_, col) => this._rows.map(row => row[col]));
+  }
+
+  get rows() {
+    return this._rows;
+  }
+
+  get columns() {
+    return this._cols;
+  }
+}
+
+module.exports = Matrix;
