27348e899931b12947dc51058d68c971d1b8ad43
laurelton <777443+laurelton@users.noreply.github.com>
2018-04-09 10:30:48 -0400

    Exclusive exercises

---
+++ bowling/bowling.js
@@ -0,0 +1,19 @@
+class Bowling {
+    constructor(rolls) {
+        this.rolls = rolls;
+    }
+
+    score = function() {
+        let score;
+        for (let i = 0; i < this.rolls.length; i++) {
+            if (rolls[i] === 10) {
+            }
+        }
+
+        return score;
+    };
+
+    roll = function(pins) {};
+}
+
+module.exports = Bowling;
--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,19 +1,21 @@
 class Matrix {
-  constructor(values) {
-    this._rows = values.split('\n').map(elem => {
-      return elem.split(' ').map(str => parseInt(str));
-    });
+    constructor(values) {
+        this._rows = values
+            .split('\n')
+            .map(elem => elem.split(' ').map(str => parseInt(str)));
 
-    this._cols = this._rows[0].map((_, col) => this._rows.map(row => row[col]));
-  }
+        this._cols = this._rows[0].map((_, col) =>
+            this._rows.map(row => row[col])
+        );
+    }
 
-  get rows() {
-    return this._rows;
-  }
+    get rows() {
+        return this._rows;
+    }
 
-  get columns() {
-    return this._cols;
-  }
+    get columns() {
+        return this._cols;
+    }
 }
 
 module.exports = Matrix;
